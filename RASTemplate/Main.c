dbfd83e5b0351a80a6151e2ee6cb25885642c6b2
josephvoss <josephvoss@utexas.edu>
2014-10-22 16:34:39 -0500

    Working on turn function, don't know what to set servo to Working on IR servo turn and read, don't know

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -7,39 +7,68 @@
 // Blink the LED to show we're on
 tBoolean blink_on = true;
 
+tMotor* rightM;
+tMotor* leftM;
+tMotor* irServo;
+tADC* frontIR;
+tADC* servoIR;
+int frontIRVolt;
+int leftVolt;
+int rightVolt;
+int turnLeft;
+const int minVoltageDis=2.00;
+
 void blink(void) {
     SetPin(PIN_F3, blink_on);
     blink_on = !blink_on;
 }
 
+void turn(turnL) {
+	if (turnL) {
+		SetMotor(leftM, 1.0f);
+		SetMotor(rightM, -1.0f);
+	}
+	else {
+		SetMotor(leftM, -1.0f);
+		SetMotor(rightM, -1.0f);
+}
+
 
 // The 'main' function is the entry point of the program
-tMotor* rightM;
-tMotor* leftM;
-tMotor* irServo;
-tADC* irSensor;
-int frontSensor;
-const int minVoltageDis;
 int main(void) {
     // Initialization code can go here
     CallEvery(blink, 0, 0.5);
 		rightM = InitializeServoMotor(PIN_B5, false);
 		leftM = InitializeServoMotor(PIN_B0, true);
 		irServo = InitializeServoMotor(PIN_B3, true);
-		SetPin(PIN_B0,true);
-		irSensor = InitializeADC(PIN_F2);
-		ADCReadContinuously(irSensor, 0.25f);	
+		//SetPin(PIN_B0,true);
+		frontIR = InitializeADC(PIN_F2);
+		ADCReadContinuously(frontIR, 0.25f);	
     while (1) {
         // Runtime code can go here
         Printf("Hello World!\n");
 				SetMotor(leftM, .25f);
 				SetMotor(rightM, .25f);
-				frontSensor=ADCRead(irSensor);
-				if (frontSensor >= minVoltageDis) {
+				frontIRVolt=ADCRead(frontIR);
+				if (frontIRVolt >= minVoltageDis) {
 					//begin IRsensor turn, not sure about distance
-					SetMotor(irServo, .25f);
+					//WAIT
+					SetMotor(irServo, -1.0f);
+					leftVolt=ADCRead(servoIR);
+					SetMotor(irServo, 1.0f);
+					rightVolt=ADCRead(servoIR);
+					SetPin(PIN_F3, true);
+					if (leftVolt > rightVolt) {
+						turnLeft=false;
+					}
+					else {
+						turnLeft=true;
+					}
+					turn(turnLeft);
 				}
-				
+				else {
+					SetPin(PIN_F3, false);
+				}
         
     }
 
