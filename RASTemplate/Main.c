5decaa51325ba3981c7c5ed0064503e0d8904685
josephvoss <josephvoss@utexas.edu>
2014-11-04 18:45:45 -0600

    draft of line follower function

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -91,33 +91,92 @@ int irServoTurn(void) {
 // The 'main' function is the entry point of the program
 int count=0;
 
+
+//First attempt at line following code. Feel free to change/alter/take apart/mutilate -J
+//NOT TESTED WITH BATTERY, or at all really
+void runLineFollower(void) {
+	LineSensorReadArray(lineSensor, lineArray); 
+	if (lineArray[0]>1.0) {
+		//turn hard left
+		SetMotor(leftM, .1f);
+		SetMotor(rightM, .15f);
+	}
+	else if(lineArray[1]>1.0) {
+		//medium left
+		SetMotor(leftM, .1f);
+		SetMotor(rightM, .134f);
+	}
+	else if(lineArray[2]>1.0) {
+		//soft left
+		SetMotor(leftM, .1f);
+		SetMotor(rightM, .117f);
+	}
+	else if(lineArray[3]>1.0) {
+		//straight
+		SetMotor(leftM, .1f);
+		SetMotor(rightM, .1f);
+	}
+	else if(lineArray[4]>1.0) {
+		//straight
+		SetMotor(leftM, .1f);
+		SetMotor(rightM, .1f);
+	}
+	else if(lineArray[5]>1.0) {
+		//soft right
+		SetMotor(leftM, .117f);
+		SetMotor(rightM, .1f);
+	}
+	else if(lineArray[6]>1.0) {
+		//medium right
+		SetMotor(leftM, .134f);
+		SetMotor(rightM, .1f);
+	}
+	else if(lineArray[7]>1.0) {
+		//hard right
+		SetMotor(leftM, .15f);
+		SetMotor(rightM, .1f);
+	}
+}
+
 int main(void) {
     // Initialization code can go here
 	Printf("Not dead");
     CallEvery(blink, 0, .25);
-	rightM = InitializeServoMotor(PIN_B5, false);
-	leftM = InitializeServoMotor(PIN_B6, true);
+	rightM = InitializeServoMotor(PIN_C7, true);
+	leftM = InitializeServoMotor(PIN_B5, false);
 	irServo = InitializeServo(PIN_B3);
 	frontIR = InitializeADC(PIN_D0);
 	servoIR = InitializeADC(PIN_D1);
 	ADCReadContinuously(frontIR, 0.25f);
 	lineSensor = InitializeGPIOLineSensor(PIN_A2, PIN_A3, PIN_A4, PIN_B6, PIN_B7, PIN_F0, PIN_E0, PIN_B2);
-    while (1) {
+  LineSensorReadContinuously(lineSensor, 0.f);
+	while (1) {
         // Runtime code can go here
-        //Printf("Hello World!\n");
-		LineSensorReadArray(lineSensor, lineArray);
-		//Printf("%02f\n", lineArray[7]);
+    //Printf("Hello World!\n");
+	  Printf("%.2f\t",lineArray[0]);
+		Printf("%.2f\t",lineArray[1]);
+		Printf("%.2f\t",lineArray[2]);
+		Printf("%.2f\t",lineArray[3]);
+		Printf("%.2f\t",lineArray[4]);
+		Printf("%.2f\t",lineArray[5]);
+		Printf("%.2f\t",lineArray[6]);
+		Printf("%.2f\n",lineArray[7]);
+
+		//go line follower function
+		//runLineFollower();
+		//THERE CAN ONLY BE ONE...comment
+		//(if un-comment runLineFollower, comment out this to avoid reading twice
+		LineSensorReadArray(lineSensor, lineArray); 
 		
-		for (i=0;i < 8;i++) {
-			if(!isinf(lineArray[i])) {
-				Printf("%.2f\t",lineArray[i]);
-			}
-		}
-		//Printf("\n");
-		//Printf("%d\n", fuck);
-		SetMotor(leftM, .25f);
-		SetMotor(rightM, .25f);
-		frontIRVolt=(int) (ADCRead(frontIR)*100);
+		//comment out motors to avoid un-desirable enhanced robotic mobility capabilities
+		//SetMotor(leftM, .1f);
+		//SetMotor(rightM, .1185f);
+		
+		
+		//Gross icky line follower stuff
+		
+		
+		//frontIRVolt=(int) (ADCRead(frontIR)*100);
 		//Printf("%02d\n", frontIRVolt);
 		
 		////////////////////////////////////////////////////////////	-Peter-	   ////////////////////////////////////////////////////
